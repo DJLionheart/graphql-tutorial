b7154bb2fd75e3d5f5d793ebd06351c6f970a8ee
DJLionheart <aundario@gmail.com>
2018-05-16 11:37:29 -0600

    initial commit

---
+++ src/example.js
@@ -0,0 +1,51 @@
+// Two main types of requests: Query and Mutation
+
+const { GraphQLServer } = require('graphql-yoga')
+
+const typeDefs = `
+type Query {
+  info: String!
+  feed: [Link!]!
+}
+
+type Mutation {
+  post(url: String!, description: String!): Link!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Mutation: {
+        post: (root, args) => {
+           const link = {
+            id: `link-${idCount++}`,
+            description: args.description,
+            url: args.url,
+          }
+          links.push(link)
+          return link
+        }
+    },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+        link: (id) => 
+    },
+
+
+    // Adding new integer variable, serves as a way to generate unique IDs for newly created Link elements.
+    Mutation: {
+        post: (root, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
+    },
+
+    // Link: {
+    //     id: (root) => root.id,
+    //     description: (root) => root.description,
+    //     url: (root) => root.url,
+    // }
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+})
+
+server.start( () => console.log(`Ye olde server doth lend an ear on http://localhost:4000`))
